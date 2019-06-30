445a9fc987ea2e543821d9feeda0f23e96aa7084
John Hellbom <john@johnhellbom.com>
2019-06-30 22:29:13 +0200

    Change to secure API connections

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://data.riksdagen.se'
+  apiUrl: 'https://data.riksdagen.se'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://data.riksdagen.se'
+  apiUrl: 'https://data.riksdagen.se'
 };
